fe11f19869fa65535fe3b2dec91136f6964bb171
Danny Pacheco <pacheco4@buffalo.edu>
2022-01-24 13:25:05 -0500

    should run now

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.resolve(__dirname, 'build')));
+app.use(express.static(path.resolve(__dirname,'..', 'build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname,'..', 'build', 'index.html'));
